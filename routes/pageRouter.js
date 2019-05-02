e4a0855c2617a81315b79e8fedb62c247a1294ff
jongholee <jhleed3@gmail.com>
2019-05-02 14:26:31 +0900

    로그인 페이지 추가

--- routes/pageRouter.js
+++ routes/pageRouter.js
@@ -5,4 +5,8 @@ router.get('/signup', function (req, res, next) {
     return res.render('signup');
 });
 
+router.get('/signin', function (req, res, next) {
+    return res.render('signin');
+});
+
 module.exports = router;
